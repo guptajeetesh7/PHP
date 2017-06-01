c0ed205492fd05fc3993043abbcd082a7b024f9e
guptajeetesh7 <gupta.jeetesh7@gmail.com>
2017-06-01 23:19:42 +0530

    Restriction added

--- upload file/main.php
+++ upload file/main.php
@@ -5,20 +5,46 @@ if(isset($_POST['submit']) && !empty($_POST['submit'])){
 	$file_name = $_FILES['file']['name'];
 	$tmp_name = $_FILES['file']['tmp_name'];
 
-	if(isset($file_name) && !empty($file_name)){
+	$size = $_FILES['file']['size'];
+	$type = $_FILES['file']['type'];
 
-		$location = "uploads/";
 
-		if(move_uploaded_file($tmp_name, $location.$file_name))
-		{
-			echo "File uploaded succesfully";
+	$extension = substr($file_name,strpos($file_name,'.' )+1);
 
-		}
-		else
-		{
-			echo "Oops Try later Server down";
-		}
+	$max_size = 2097152 ;
 
+	 $extension = strtolower($extension);
+
+	//	 echo $type.$size;
+
+	if(isset($file_name) && !empty($file_name) ){
+
+		if( ($extension=='jpg' || $extension=='jpeg') && ($type=='image/jpeg' || $type=='image/jpg') ){
+
+			if($size < $max_size){
+
+									$location = 'uploads/';
+
+									if(move_uploaded_file($tmp_name,$location.$file_name))
+											{
+												echo "File uploaded succesfully";
+
+											}
+										else
+											{
+												echo "Oops Try later Server down";
+											}
+
+								}
+
+
+						else{
+								echo "Size must be less than 2MB";
+							}
+	}
+	else{
+		echo "Please upload a jpeg or jpg file";
+	}
 
 		
 	}
